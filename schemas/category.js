304b4961b9f9668b5281a471bb30c9bf47ad816a
ethan <ethanzhou762gmail.com>
2021-06-20 19:05:56 -0400

    Made backend able to change colour of tag

--- schemas/category.js
+++ schemas/category.js
@@ -9,6 +9,37 @@ export default {
       type: 'string',
     },
     {
+      name: 'slug',
+      title: 'URL',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'colour',
+      title: 'Colour',
+      type: 'string',
+      options: {
+        list: [
+          'red',
+          'orange',
+          'yellow',
+          'olive',
+          'green',
+          'teal',
+          'blue',
+          'violet',
+          'purple',
+          'pink',
+          'brown',
+          'grey',
+          'black',
+        ],
+      },
+    },
+    {
       name: 'description',
       title: 'Description',
       type: 'text',
